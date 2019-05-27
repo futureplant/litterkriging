d0b6b5356992fb1d8b9ea484cd63b0a7c0e8ab2f
future_plant <hodde.j@gmail.com>
2019-05-27 09:51:36 +0200

    added pseudocode

--- main.R
+++ main.R
@@ -2,3 +2,41 @@
 # Main script for litterkriging
 # Use as bookkeeping script, refer to /scripts for logic
 
+
+# Get data from google sheets
+  # temporarily simulated data
+
+
+
+# Turn data into spatial object (sp)
+
+
+
+
+# Make semivariogram
+
+
+
+
+# Export semivariogram to png
+
+
+
+
+# Perform Kriging (euclidian or linear network)
+
+
+
+
+# (optional) Clip Raster on buffered roads
+
+
+
+
+
+# Visualize 
+
+
+
+
+# Export to geotiff
