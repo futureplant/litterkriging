5f2163f6310e2e327d39e2394c777463cbf285b4
future_plant <hodde.j@gmail.com>
2019-06-11 13:36:58 +0200

    added inter operator variability code

--- inter_operator_variability.R
+++ inter_operator_variability.R
@@ -1,1 +1,16 @@
 # inter operator variability is calculated here
+library(gsheet)
+library(dplyr)
+
+sampledata <- getDataFrame('https://docs.google.com/spreadsheets/d/1Dn96ArmKeIu-lnDSUHzAKnGcJv7Kjmqii_H-Y-zVd74/edit?usp=sharing')
+bias <- sampledata[sampledata$Point.ID >= 400,]
+
+bias <- select(bias,c(Point.ID, Your.name, total))
+bias$total <- as.numeric(bias$total)
+bias$Point.ID <- as.numeric(bias$Point.ID)
+plot(bias$Point.ID,bias$total)
+
+bias <- split(bias,bias$Point.ID)
+bias$`401`
+
+
