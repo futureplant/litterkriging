aafd4b4e84322400d78cd5b5a6a050b71b115c99
Melanie <melaniearp@hotmail.com>
2019-06-14 14:18:51 +0200

    Added datasets necessary for sampling script and reorganized the sampling script

--- main_sampling.R
+++ main_sampling.R
@@ -21,38 +21,39 @@ library(raster)
 
 
 # Import road networks and study area ------------------------------------
-roadnetwork <- readOGR(dsn = "data", layer = "osm_roads_aoi_wgs84")
+roadnetwork <- readOGR(dsn = "data", layer = "osm_roads_groenlo")
 
 study_area <- readOGR(dsn = "data", layer = "mapping_area_groenlo")
 
 study_roadnetwork <- crop(roadnetwork, study_area)
 
-## KRIGING POINTS ## -----------------------------
 
 # Test spsample -----------------------------------------
 test_road <- readOGR(dsn = "data", layer = "Test_road")
 plot(test_road)
-test_samples <- spsample(test_road, 10, type = 'regular')
+test_samples <- spsample(test_road, n = 10, type = 'regular')
 plot(test_samples, add = T, col = 'red')
 
+
+## KRIGING SAMPLING ## -----------------------------
 
 # Create a regular grid along road ------------------------------------
 sample_reg <- spsample(study_roadnetwork, n = 90, type = "regular")
 length(sample_reg)
 
 id_df = as.data.frame((1:length(sample_reg)))
-id_df[1:74,] = as.numeric(id_df[1:74,])
+id_df[1:length(sample_reg),] = as.numeric(id_df[1:length(sample_reg),])
 names(id_df) = 'ID'
 sample_reg <- SpatialPointsDataFrame(sample_reg, id_df)
 
-plot(study_roadnetwork) #, col = study_roadnetwork@data$code)
+plot(study_roadnetwork) 
 plot(sample_reg, add = T, col = 'red')
 
 # Export regular grid sampling points
 writeOGR(sample_reg, 'output', layer = 'regular_sample_points', driver = 'ESRI Shapefile')
 
 
-# Create short-distance random coordinates ------------------------------
+# Create short-distance random distribution ------------------------------
 set.seed(500)
 sample_short <- sample(1:nrow(sample_reg@coords), 30)
 
@@ -82,8 +83,7 @@ writeOGR(sample_points_wgs84, 'output', layer = 'sample_points', driver = 'ESRI 
 
 
 
-## VALIDATION POINTS ## -----------------------------
-set.seed(800)
+## VALIDATION SAMPLING ## -----------------------------
 validation_points <- spsample(study_roadnetwork, n = 50, type = "random")
 length(validation_points)
 
@@ -96,28 +96,22 @@ validation_points <- SpatialPointsDataFrame(validation_points, val_df)
 writeOGR(validation_points, 'output', layer = 'validation_points', driver = 'ESRI Shapefile')
 
 
-
-
-
-
-
 ## PERSONAL BIAS SAMPLING ## --------------------------------
-# Import road networks outside of original study area
-roadnetwork <- readOGR(dsn = "data", layer = "extra_roads_wgs84")
-plot(roadnetwork)
+# Import road networks outside of original study area -----------------------------------
+roadnetwork_bias <- readOGR(dsn = "data", layer = "extra_roads_wgs84")
+plot(roadnetwork_bias)
 
-# Create random sampling points
-set.seed(850)
-sample_rndm <- spsample(roadnetwork, n = 11, type = "random")
-length(sample_rndm)
+# Create random sampling points ----------------------------------------
+sample_bias <- spsample(roadnetwork, n = 11, type = "random")
+length(sample_bias)
 
-plot(sample_rndm, add = T, col = 'red')
+plot(sample_bias, add = T, col = 'red')
 
-id_df = as.data.frame((1:length(sample_rndm)))
+id_df = as.data.frame((1:length(sample_bias)))
 id_df[1:nrow(id_df),] = as.numeric(id_df[1:nrow(id_df),])
 names(id_df) = 'ID'
-sample_rndm <- SpatialPointsDataFrame(sample_rndm, id_df)
+sample_bias <- SpatialPointsDataFrame(sample_bias, id_df)
 
 
 # Export points for personal bias investigation
-writeOGR(sample_rndm, 'output', layer = 'bias_samples', driver = 'ESRI Shapefile')
+writeOGR(sample_bias, 'output', layer = 'bias_samples', driver = 'ESRI Shapefile')
